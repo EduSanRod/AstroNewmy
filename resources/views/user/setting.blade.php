2d5a7c8dc9d39ff332f5a3d8652726f91b931a67
EduSanRod <edusanchez4bc@gmail.com>
2022-07-13 15:19:58 +0200

    revamp user settings

--- resources/views/user/setting.blade.php
+++ resources/views/user/setting.blade.php
@@ -3,32 +3,27 @@
 @section('head')
 
 <title>{{ Auth::user()->name; }} Profile</title>
-
+<link rel="stylesheet" type="text/css" href="{{ asset('css/user/settings.css') }}">
 @endsection
 
 @section('section')
 
-<div class="card-body p-4">
-	<h2>User Information</h2>
-	<hr class="mt-0 mb-4">
-	<div class="row pt-1">
-		<form class="row pt-1" action="{{ route('user.update') }}" method="post">
+<div id="profile-container">
+	<h2>{{ Auth::user()->name; }} Profile</h2>
+	<form action="{{ route('user.update') }}" method="post">
 		@csrf
-			<div class="col-6 mb-3 inline">
-				<h3>Username</h3>
-				<input type="text" id="username" name="username" class="form-control" minlength="4" maxlength="15" placeholder="{{ Auth::user()->name; }}"/>
-			</div>
-			<div class="col-6 mb-3 inline">
-				<h3>Email</h3>
-				<input type="email" id="email" name="email" class="form-control" placeholder="{{ Auth::user()->email; }}"/>
-			</div>
 
-			<input type="submit" class="btn btn-primary btn-block m-3 w-25 " value="Save changes">
-		</form>
+		<h3>Username</h3>
+		<input type="text" id="username" name="username" class="form-input" minlength="4" maxlength="15" placeholder="{{ Auth::user()->name; }}" />
 
-		<a class="btn btn-danger btn-block w-25" href="{{ route('user.delete') }}">Delete account</a>
-		
-	</div>
+		<h3>Email</h3>
+		<input type="email" id="email" name="email" class="form-input" placeholder="{{ Auth::user()->email; }}" />
+
+		<input type="submit" class="submit-button" value="Save changes">
+	</form>
+
+	<a href="{{ route('user.delete') }}" class="delete-button" title="Delete account">Delete account</a>
+
 </div>
 
 @endsection
